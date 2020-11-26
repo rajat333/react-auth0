47f3eb7ac4f26e18db81aa04441408ab618d1c0e
Rajat khurana <Rajat khuranarajatkhurana@gmail.com>
2020-11-27 01:33:52 +0530

    Profile View

--- api-server.js
+++ api-server.js
@@ -5,9 +5,23 @@ const helmet = require("helmet");
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 const authConfig = require("./src/auth_config.json");
+const mysql = require('mysql');
+const bodyParser = require('body-parser')
 
 const app = express();
 
+const connection = mysql.createConnection({
+  connectionLimit: 5,
+  host: 'sql12.freemysqlhosting.net',
+  user: 'sql12378623',
+  password: 'PBQp9vimSp',
+  database: 'sql12378623'
+});
+connection.connect((err) => {
+  console.log('db connection');
+  if (err) throw err;
+  console.log('Connected!');
+});
 const port = process.env.API_PORT || 3001;
 const appPort = process.env.SERVER_PORT || 3000;
 const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
@@ -21,6 +35,11 @@ if (!authConfig.domain || !authConfig.audience) {
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors({ origin: appOrigin }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+const jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -36,9 +55,116 @@ const checkJwt = jwt({
 });
 
 app.get("/api/external", checkJwt, (req, res) => {
+  console.log('req req',req.user);
   res.send({
     msg: "Your access token was successfully validated!"
   });
+});
+function isUserExist (auth0Id){
+  const sql = `Select * from User where auth0Id = ${ mysql.escape(auth0Id) }`
+  return new Promise( function(resolve, reject) {
+    connection.query(sql,function (err, result, fields) {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+  })
+};
+
+function insertUser (name, auth0Id, profileDescription, email, city){
+  const sql = `Insert Into User (name, auth0Id, profileDescription, email, city) 
+   Values = ${ mysql.escape(name),mysql.escape(auth0Id),
+     mysql.escape(profileDescription), mysql.escape(email), mysql.escape(city) }`
+  return new Promise( function(resolve, reject) {
+    connection.query(sql,function (err, result, fields) {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+  })
+};
+
+function updateUser (body, auth0Id ){
+  const sql = `UPDATE customers SET name = ${  mysql.escape(name) }, 
+   profileDescription = ${ mysql.escape(profileDescription) }, 
+   city = ${  mysql.escape(city) }, 
+   WHERE auth0Id = ${  mysql.escape(auth0Id) }`;
+
+  return new Promise( function(resolve, reject) {
+    connection.query(sql,function (err, result, fields) {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+  })
+};
+
+function updateSkills (skills ){
+
+  let removeSkill = skills.filter( e => (e.removed === true));
+  // let newSkill = 
+  const sql = `UPDATE customers SET name = ${  mysql.escape(name) }, 
+   profileDescription = ${ mysql.escape(profileDescription) }, 
+   city = ${  mysql.escape(city) }, 
+   WHERE auth0Id = ${  mysql.escape(auth0Id) }`;
+
+  return new Promise( function(resolve, reject) {
+    connection.query(sql,function (err, result, fields) {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+  })
+};
+
+
+app.post("/api/profileUpdate", jsonParser, checkJwt, (req, res) => {
+  const body = req.body;
+  // console.log('req req ', req);
+  const auth0Id = req.user.sub.split('|')[1];
+  console.log('auth0Id', auth0Id, body);
+  isUserExist( auth0Id)
+  .then( result=>{  
+      console.log(' result >>>>>>>>>>>>', result);
+      if( result.length === 0){
+        console.log(' new user ');
+        // new user 
+          return insertUser(body.name,auth0Id, body.profileDescription,body.email, body.city);
+      }else{
+        // update user 
+        console.log(' existing user update ')
+        return updateUser(body, auth0Id);
+      }
+      
+  }).then( userDetails =>{
+      // insert skills based on user
+      console.log(' user details ', userDetails);
+      if( body.skills) {
+          return updateSkills(body.skills);
+      }
+
+  }).catch(e=>{
+    console.log('Exception occur', e);
+  })
+  
+  // connection.query("SELECT * FROM user ", function (err, result, fields) {
+  //   if (err) throw err;
+  //   console.log('###########', result);
+  // });
+  // var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+
+  // var sql = "SELECT * FROM User JOIN Skill ON users.userId = Skill.userId";
+
+  // var sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
+  // con.query(sql, function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result.affectedRows + " record(s) updated");
+  // });
+
 });
 
 app.listen(port, () => console.log(`API Server listening on port ${port}`));
--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,30 +1,131 @@
-import React from "react";
+import React ,  { useState , useEffect} from "react";
 import { Container, Row, Col } from "reactstrap";
-
-import Highlight from "../components/Highlight";
+import { Badge, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+// import Highlight from "../components/Highlight";
 import Loading from "../components/Loading";
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
 export const ProfileComponent = () => {
   const { user } = useAuth0();
+  // const [fetchedData, setFetchedData] = useState('');
+  const [email, updateEmail] = useState();
+  const [city, updateCity] = useState();
+  const [name, updateName] = useState();
+  const [skills, updateSkills] = useState([]);
 
+  useEffect(() => {
+    async function fetchBooks() {
+      const response = await fetch('https://reqres.in/api/users/2');
+      const json = await response.json();
+      console.log('json json ', json.data.email);
+      updateEmail(json.data.email);
+      updateCity('Jind');
+      console.log('user user user', user);
+      const skills = ["Node", "C++", "ReactJs"];
+      updateSkills(skills);
+      updateName(user.nickname);
+  }
+  fetchBooks();
+  },[]);
+
+  const handleSubmit = e => {
+    console.log(' handle submit ',e);
+    e.preventDefault();
+    console.log('22222222222222');
+  }
   return (
     <Container className="mb-5">
-      <Row className="align-items-center profile-header mb-5 text-center text-md-left">
-        <Col md={2}>
-          <img
-            src={user.picture}
-            alt="Profile"
-            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-          />
-        </Col>
-        <Col md>
-          <h2>{user.name}</h2>
-          <p className="lead text-muted">{user.email}</p>
-        </Col>
-      </Row>
       <Row>
-        <Highlight>{JSON.stringify(user, null, 2)}</Highlight>
+        {/* <Highlight>{JSON.stringify(user, null, 2)}</Highlight> */}
+        <Form onSubmit={ handleSubmit }>
+        <FormGroup>
+        <Label for="name">Name </Label>
+        <Input type="text"  value={ name }
+        name="name" id="name" placeholder="Enter your name" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleEmail">Email </Label>
+        <Input type="email"  value={ email }
+        name="email" id="exampleEmail" placeholder="with a placeholder" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="city">City </Label>
+        <Input type="text"  value={ city }
+        name="city" id="city" placeholder="with a placeholder" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Skills</Label>
+        <Input type="text" name="skill" id="skill" placeholder="Enter new skills" />
+        {
+          skills.map( e =>{
+              return (
+                <h6 key={ e+Math.random() }> <Badge color="secondary">{ e }</Badge> </h6>
+              )
+          })
+        }
+      </FormGroup>
+      {/* <FormGroup>
+        <Label for="exampleSelect">Select</Label>
+        <Input type="select" name="select" id="exampleSelect">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </Input>
+      </FormGroup> */}
+      {/* <FormGroup>
+        <Label for="exampleSelectMulti">Select Multiple</Label>
+        <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </Input>
+      </FormGroup> */}
+      {/* <FormGroup>
+        <Label for="exampleText">Text Area</Label>
+        <Input type="textarea" name="text" id="exampleText" />
+      </FormGroup> */}
+      {/* <FormGroup>
+        <Label for="exampleFile">File</Label>
+        <Input type="file" name="file" id="exampleFile" />
+        <FormText color="muted">
+          This is some placeholder block-level help text for the above input.
+          It's a bit lighter and easily wraps to a new line.
+        </FormText>
+      </FormGroup> */}
+      {/* <FormGroup tag="fieldset">
+        <legend>Radio Buttons</legend>
+        <FormGroup check>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+            Option one is this and that—be sure to include why it's great
+          </Label>
+        </FormGroup> */}
+        {/* <FormGroup check>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+            Option two can be something else and selecting it will deselect option one
+          </Label>
+        </FormGroup> */}
+        {/* <FormGroup check disabled>
+          <Label check>
+            <Input type="radio" name="radio1" disabled />{' '}
+            Option three is disabled
+          </Label>
+        </FormGroup> */}
+      {/* </FormGroup> */}
+      {/* <FormGroup check>
+        <Label check>
+          <Input type="checkbox" />{' '}
+          Check me out
+        </Label>
+      </FormGroup> */}
+      <Button>Submit</Button>
+    </Form>
+
       </Row>
     </Container>
   );
